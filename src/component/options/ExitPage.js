3e655cc35d4d0321cdbd5d24068d2e563d6faae2
Athul <athulmaliyakkal@gmail.com>
2023-08-13 01:43:01 +0530

    Update

--- src/component/options/ExitPage.js
+++ src/component/options/ExitPage.js
@@ -1,23 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { addAge } from '../../redux/Action';
-const ExitPage = (props) => {
+const ExitPage = () => {
 
     const [count,setCount]=useState(5);
     useEffect(()=>{
-        setTimeout(()=> {
-
+        const timer=setTimeout(()=> {
           setCount(count-1);
             if(count===2)
             window.location.replace('/exit');
           }, 1000);
-
+          return ()=>clearTimeout(timer);
     },[count]);
   return (
     <div style={{color:'white'}}>
         {`${count}..`}
     </div>
-  )
+  ) 
 }
-
 export default ExitPage
