e4f5ceaae086338003c966401a79ff80bc6ab653
Athul <athulmaliyakkal@gmail.com>
2023-08-13 01:40:48 +0530

    Update

--- src/component/chatbot/config.js
+++ src/component/chatbot/config.js
@@ -1,19 +1,18 @@
-
-// Config starter code
-import { createChatBotMessage } from "react-chatbot-kit";
+import { createChatBotMessage} from "react-chatbot-kit";
 import Options from "../options/Options";
-import Name from "../options/Name";
 import Age from "../options/Age";
 import ExitPage from "../options/ExitPage";
 const config = {
   botName:"YourBot",
   initialMessages: [createChatBotMessage(`Hello, Welcome to student info system!`,
   {widget:"options"})],
+  customComponents: {},
   widgets: [
     {
       widgetName: "options",
       widgetFunc: (props) => <Options {...props} />,
     },
+    
     {
       widgetName: "age",
       widgetFunc: (props) => <Age {...props} />,
@@ -23,7 +22,7 @@ const config = {
       widgetFunc: (props) => <ExitPage {...props} />,
     },
   ],
-
+  // customComponents:
 }
 
 export default config
