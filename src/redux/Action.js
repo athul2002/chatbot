64631ff45a6434f1eac420606c33a704048cfe22
Athul <athulmaliyakkal@gmail.com>
2023-08-13 01:41:51 +0530

    Update

--- src/redux/Action.js
+++ src/redux/Action.js
@@ -1,21 +1,13 @@
-// import { type } from "@testing-library/user-event/dist/type";
 import { AGE,NAMES } from "./constants";
-
-
 export const addAge=(age)=>async(dispatch)=>{
-    
         dispatch({
             type:AGE,
             ages:age
         });
-        localStorage.setItem("age", JSON.stringify(age));
 }
 export const addName=(name)=>async(dispatch)=>{
-    // console.log(name)
         dispatch({
             type:NAMES,
             names:name
-        });
-        localStorage.setItem("name", JSON.stringify(name));
-    
+        }); 
 }
